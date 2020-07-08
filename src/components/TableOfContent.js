1e6aa7d86ed3ac64d39f8073b3015a370908874b
Anna Ellerbrake <anna.ellerbrake@schullv.de>
2020-07-08 12:26:20 +0200

    Pull code for styled table of content out of booklist component file

--- src/components/TableOfContent.js
+++ src/components/TableOfContent.js
@@ -1,13 +1,13 @@
 import React from 'react'
-import { StyledList } from './BookList'
+import styled from 'styled-components'
 
 export default function TableOfContent({ bookChapters }) {
   return (
-    <StyledList>
+    <StyledTableOfContent>
       {bookChapters &&
         bookChapters.length > 0 &&
         bookChapters.map((chapter) => renderItem(chapter))}
-    </StyledList>
+    </StyledTableOfContent>
   )
 
   function renderItem(item) {
@@ -29,3 +29,54 @@ export default function TableOfContent({ bookChapters }) {
     }
   }
 }
+
+export const StyledTableOfContent = styled.ul`
+  margin: 4px auto 0;
+  padding: 0;
+  text-align: center;
+  overflow: scroll;
+  height: 55vh;
+  width: 80%;
+  
+  li {
+    text-align: left;
+    list-style: none;
+    display: block;
+    margin: 2px auto;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+  }
+
+  > li {
+    
+    background-color: #82a9de;
+    padding: 13px;
+  }
+
+  > ul {
+    margin: 0 auto;
+    padding: 0;
+    text-align: center;
+
+    > li {
+      
+      background-color: #B1C9EA;
+      padding: 13px 13px 13px 35px;
+      
+    }
+
+    > ul {
+      margin: 0 auto;
+      padding: 0;
+      text-align: center;
+  
+      > li {
+        
+        background-color: #E1EAF7;
+        padding: 13px 13px 13px 57px;
+      
+        
+      }
+  }
+`

1fda1366456391aa9c908587e936e1f7d61aebbb
Anna Ellerbrake <anna.ellerbrake@schullv.de>
2020-07-11 17:07:59 +0200

    Remove comments

--- src/components/BookListItem.test.js
+++ src/components/BookListItem.test.js
@@ -6,22 +6,17 @@ import { render, fireEvent } from '@testing-library/react'
 import bookList from '../../public/containers.json'
 
 it('triggers an event on click', async () => {
-  /** create an function mock to spy on calls */
   const onSpy = jest.fn()
 
-  /** get the first data set from the api mocks */
   const bookData = bookList[0]
 
   const { getByTestId } = render(
     <BookListItem book={bookData} onClick={onSpy} />
   )
 
-  /** Get the Button by the testid */
   const button = getByTestId('book-button')
 
-  /** Trigger the click event of the button */
   fireEvent.click(button)
 
-  /** Check if onSpy was triggered one time */
   expect(onSpy).toHaveBeenCalledTimes(1)
 })
--- src/components/Dropdown.test.js
+++ src/components/Dropdown.test.js
@@ -2,12 +2,6 @@ import React from 'react'
 import { render, fireEvent } from '@testing-library/react'
 import Dropdown from './Dropdown'
 
-// it('renders a div with the text Wähle eine Lektürehilfe', () => {
-//   const div = document.createElement('div')
-//   ReactDOM.render(<Dropdown />, div)
-//   expect(div.querySelector('div').textContent).toBe('Wähle eine Lektürehilfe')
-// })
-
 it('exists an text content Wähle eine Lektürehilfe', () => {
   const { queryByText } = render(
     <Dropdown selectText={'Wähle eine Lektürehilfe'} />
@@ -16,19 +10,12 @@ it('exists an text content Wähle eine Lektürehilfe', () => {
 })
 
 it('triggers an event on click', async () => {
-  /** create an function mock to spy on calls */
   const onSpy = jest.fn()
-
-  /** get the first data set from the api mocks */
-
   const { getByTestId } = render(<Dropdown onClick={onSpy} />)
 
-  /** Get the Button by the testid */
   const button = getByTestId('dropdown-button')
 
-  /** Trigger the click event of the button */
   fireEvent.click(button)
 
-  /** Check if onSpy was triggered one time */
   expect(onSpy).toHaveBeenCalledTimes(1)
 })

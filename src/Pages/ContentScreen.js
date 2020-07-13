1ed6c0a5a6a712df725918e1cc6220bc1ee875ca
Anna Ellerbrake <anna.ellerbrake@schullv.de>
2020-07-13 15:56:33 +0200

    Add layout styling for header on contentscreen

--- src/Pages/ContentScreen.js
+++ src/Pages/ContentScreen.js
@@ -3,6 +3,8 @@ import { useState, useEffect } from 'react'
 import axios from 'axios'
 import styled from 'styled-components'
 import { useParams } from 'react-router-dom'
+import NavigateBeforeIcon from '@material-ui/icons/NavigateBefore'
+import { NavLink } from 'react-router-dom'
 
 export default function ContentScreen() {
   const props = useParams()
@@ -21,6 +23,9 @@ export default function ContentScreen() {
   return (
     <Grid>
       <header>
+        <NavLink to="/">
+          <NavigateBeforeIcon size={48} />
+        </NavLink>
         <h1>{props.topic}</h1>
       </header>
       <section dangerouslySetInnerHTML={{ __html: content.html }} />
@@ -33,19 +38,36 @@ const Grid = styled.div`
   height: 100vh;
   display: grid;
   grid-template-rows: 80px auto;
-  justify-content: center;
+  justify-content: space-between;
   align-items: center;
 
   > header {
     display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
+    flex-direction: row;
     color: var(--primaryBlue);
-    font-size: 0.8rem;
+    width: 100vw;
+    text-align: center;
+
+    justify-content: center;
+
+    > a {
+      position: absolute;
+      left: 15px;
+      top: 29px;
+      justify-self: start;
+
+      text-decoration: none;
+      color: var(--primaryBlue);
+      transform: scale(1.4);
+    }
 
     > h1 {
-      display: block;
+      margin: 0;
+      margin-left: 50px;
+      margin-right: 50px;
+      font-size: 1.5rem;
+      justify-content: center;
+      align-self: start;
       overflow: hidden;
       text-overflow: ellipsis;
       white-space: nowrap;
--- src/components/TableOfContent.js
+++ src/components/TableOfContent.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
-import { Link, NavLink } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 export default function TableOfContent({ bookChapters }) {
   return (
